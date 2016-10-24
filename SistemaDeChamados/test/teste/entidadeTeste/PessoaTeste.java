31435de992ee3aac81cd27b8eb82d2336f4596ce
DenisSlapelis <>
2016-10-24 21:50:52 -0200

    Adicionado algumas classes de teste e implementação da classe PessoaTeste

---
+++ SistemaDeChamados/test/teste/entidadeTeste/PessoaTeste.java
@@ -0,0 +1,33 @@
+package teste.entidadeTeste;
+
+
+import entidade.Pessoa;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Denis
+ */
+public class PessoaTeste {
+    
+    public PessoaTeste() {
+    }
+    
+      @Test
+    public void instanciacaoTest(){
+        Pessoa p = new Pessoa("Denis", 998877665);
+        Assert.assertEquals(998877665, p.getTelefone());
+        Assert.assertEquals("Denis", p.getNome());
+    }
+    
+    public void settersTest(){
+        Pessoa p = new Pessoa("Denis", 998877665);
+        Assert.assertEquals(998877665, p.getTelefone());
+        Assert.assertEquals("Denis", p.getNome());
+        p.setNome("Wesley");
+        p.setTelefone(566778899);
+        Assert.assertEquals("Wesley", p.getNome());
+        Assert.assertEquals(566778899, p.getTelefone());
+    }
+}
